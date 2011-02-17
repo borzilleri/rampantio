9dc6bb14960cb6008e3ba0f4b50f0be09490bb35
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-02-17 13:29:38 -0800

    Add style.css, and some minor styling

--- public/index.php
+++ public/index.php
@@ -2,18 +2,21 @@
 <html>
 <head>
 	<title>Rampant I/O</title>
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
 </head>
 <body>
 
-<p><em>In the two hundred and fifty years since Rampancy first appeared 
+<div id="quote">
+	<em>In the two hundred and fifty years since Rampancy first appeared 
 [&hellip;], the stable Rampant AI, the 'Holy Grail' of cybertonics, has 
 never come close to fruition. Since no Rampant has ever been controlled 
 or turned to any useful purpose, it is the opinion of this writer and of 
 the majority of the Cybertonic community that all rampant AIs are a danger 
 to Cyberlife, Liberty, and the Pursuit of Thrashedness.</em>
-<br>
-&mdash; James B. Miller, 2320, "Life and Death of Intelligence"
-</p>
+	<div id="source">
+		&mdash; James B. Miller, 2320, "Life and Death of Intelligence"
+	</div>
+</div>
 
 </body>
 </html>
