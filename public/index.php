76a6d494da3e223decb4e716119cbdf3667e493e
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-02-17 11:58:38 -0800

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rampant I/O</title>
+</head>
+<body>
+
+<p><em>In the two hundred and fifty years since Rampancy first appeared 
+[&hellip;], the stable Rampant AI, the 'Holy Grail' of cybertonics, has 
+never come close to fruition. Since no Rampant has ever been controlled 
+or turned to any useful purpose, it is the opinion of this writer and of 
+the majority of the Cybertonic community that all rampant AIs are a danger 
+to Cyberlife, Liberty, and the Pursuit of Thrashedness.</em>
+<br>
+&mdash; James B. Miller, 2320, "Life and Death of Intelligence"
+</p>
+
+</body>
+</html>
